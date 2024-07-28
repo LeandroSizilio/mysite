300eebedd92581d9e0b8a82c7c7a54572097d184
Leandro Sizilio <lesizilio@gmail.com>
2024-07-28 15:59:33 -0300

    teste2

--- enquetes/admin.py
+++ enquetes/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
+from .models import Pergunta
 
-from .models import Pergunta, Alternativa
 
 admin.site.register(Pergunta)
-admin.site.register(Alternativa)
--- enquetes/urls.py
+++ enquetes/urls.py
@@ -1,18 +1,15 @@
 from django.urls import path
 from . import views
 
-app_name = 'enquetes'
 urlpatterns = [
-    # URL "/enquetes/" --> lista geral das enqquetes
-    path('', views.IndexView.as_view(), name='index'),
-    # URL "/enquetes/55/" --> detalhes da enquete com "id" 55
-    path(
-        '<int:pk>/',
-        views.DetalhesView.as_view(), name='detalhes'
-    ),
-    # URL "/enquetes/77/resultado/ --> resultado da enquete 77
-    path(
-        '<int:pk>/resultado/',
-        views.ResultadoView.as_view(), name='resultado'
-    ),
+    path('', views.index, name='index'),
+
+    path('<int:pergunta_id>/', views.detalhes, name='detalhes'),
+
+    path('<int:pergunta_id>/resultado/', views.resultados, name='resultado'),
+
+    path('<int:pergunta_id>/votacao/', views.votacao, name='votacao'),
+
+    path('teste/', views.teste, name='Teste'),
+
 ]
